45a964d6c4f83ab23c8ca0c251641f3f3e7a2655
LeywinS <backary.sou@gmail.com>
2024-02-09 14:42:46 +0100

    Connect to the db

--- server/index.js
+++ server/index.js
@@ -1,4 +1,15 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("MongoDB is connected");
+  })
+  .catch((e) => {
+    console.log("Error !!", e.message);
+  });
 const app = express();
 
 app.listen(3000, () => {
