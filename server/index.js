a6ac8a611260de5c68c6da18e80c5bdc2f237852
LeywinS <backary.sou@gmail.com>
2024-02-09 15:32:54 +0100

    Create signup api route

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
-import express from "express";
+import express, { json } from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
 import morgan from "morgan";
+import bodyParser from "body-parser";
 dotenv.config();
 
 mongoose
@@ -15,9 +17,12 @@ mongoose
   });
 const app = express();
 
+app.use(json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 
 app.use("/api/user", userRoutes);
+app.use("/api", authRoutes);
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
