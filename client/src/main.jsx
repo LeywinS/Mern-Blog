34aec4f853f8a0ba551f04605a53adc6666bafbd
LeywinS <backary.sou@gmail.com>
2024-02-10 11:35:43 +0100

    Add dark mode

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,11 +4,14 @@ import "./index.css";
 import { store, persistor } from "./redux/store.js";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
+import ThemeProvider from "./components/ThemeProvider.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <Provider store={store}>
-    <PersistGate persistor={persistor}>
-      <App />
-    </PersistGate>
-  </Provider>
+  <PersistGate persistor={persistor}>
+    <Provider store={store}>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </Provider>
+  </PersistGate>
 );
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -2,16 +2,18 @@ import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import { persistReducer, persistStore } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import userReducer from "./user/userSlice.js";
+import themeReducer from "./theme/themeSlice.js";
 
 const persistConfig = {
   key: "root",
   version: 1,
   storage,
-  whitelist: ["user"],
+  whitelist: ["user", "theme"],
 };
 
 const rootReducer = combineReducers({
   user: userReducer,
+  theme: themeReducer,
 });
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
