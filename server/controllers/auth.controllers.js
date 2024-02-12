7c0b82ca38e5b5043c871025273a197c61db6029
LeywinS <backary.sou@gmail.com>
2024-02-12 10:45:42 +0100

    add admin fonctionnality

--- server/controllers/auth.controllers.js
+++ server/controllers/auth.controllers.js
@@ -46,6 +46,7 @@ export const signin = async (req, res, next) => {
     const token = jwt.sign(
       {
         id: validUser._id,
+        isAdmin: validUser.isAdmin,
       },
       process.env.JWT_SECRET
     );
@@ -65,7 +66,10 @@ export const google = async (req, res, next) => {
   try {
     const user = await User.findOne({ email });
     if (user) {
-      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+      const token = jwt.sign(
+        { id: user._id, isAdmin: user.isAdmin },
+        process.env.JWT_SECRET
+      );
       const { password, ...rest } = user._doc;
       res
         .status(200)
